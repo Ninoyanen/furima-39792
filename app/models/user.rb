f2b35947d91351be328541703ab35edc43dcb3a6
Ninoyanen <s.ninomiya0821@gmail.com>
2023-12-08 16:41:15 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+  validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+
+  with_options presence: true do
+    validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "/全角で入力してください"}
+    validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "/全角で入力してください"}
+    validates :last_name_kana, format: {with: /\A[ァ-ヶ一]+\z/, message: "/全角カタカナで入力してください"}
+    validates :first_name_kana, format: {with: /\A[ァ-ヶ一]+\z/, message: "/全角カタカナで入力してください"}
+  end
+
+  validates :birth, presence: true
+
+  has_many :items
+  has_many :orders
+
+end
